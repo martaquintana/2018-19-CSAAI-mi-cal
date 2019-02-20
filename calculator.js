34364d250541f1d2c88a424777ea27f94c49c8b1
martaquintana <Marta@MacBook-Pro-de-Marta.local>
2019-02-20 17:25:53 +0100

    optimizando calculadora y añadiendo operaciones

--- calculator.js
+++ calculator.js
@@ -6,10 +6,45 @@ function operador(display){
       this.display.innerHTML =this.valor;
     }
 }
+
+function numero(operador){
+    botom0.onclick = () => {
+        operador.inc(0);
+        console.log(operador)
+    }
+    botom1.onclick = () => {
+        operador.inc(1);
+    }
+    botom2.onclick = () => {
+        operador.inc(2);
+    }
+    botom3.onclick = () => {
+        operador.inc(3);
+    }
+    botom4.onclick = () => {
+    operador.inc(4);
+    }
+    botom5.onclick = () => {
+    operador.inc(5);
+    }
+    botom6.onclick = () => {
+    operador.inc(6);
+    }
+    botom7.onclick = () => {
+    operador.inc(7);
+    }
+    botom8.onclick = () => {
+    operador.inc(8);
+    }
+    botom9.onclick = () => {
+    operador.inc(9);
+    }
+}
+
 function main()
 {
     console.log("Que comienzen los juegos de Javascript!!");
-    var cadena = '';
+    var cadena = [''];
     var botom0 = document.getElementById('botom0');
     var botom1 = document.getElementById('botom1');
     var botom2 = document.getElementById('botom2');
@@ -22,84 +57,56 @@ function main()
     var botom9 = document.getElementById('botom9');
     var display= document.getElementById('display');
     var mas = document.getElementById('mas');
+    var menos = document.getElementById('menos');
+    var multiplica= document.getElementById('multiplica');
     var igual= document.getElementById('igual');
     var ac = document.getElementById('ac');
 
     var operador1 = new operador(display)
     var operador2= new operador(display)
+    numero(operador1)
+    mas.onclick =()=>{
+          cadena [0] = operador1.valor;
+          cadena[1] = "+"
+          numero(operador2);
+    }
+    menos.onclick =()=>{
+          cadena [0] = operador1.valor;
+          cadena[1] = "-"
+          numero(operador2);
+    }
+    multiplica.onclick =()=>{
+          cadena [0] = operador1.valor;
+          cadena[1] = "x"
+          numero(operador2);
+    }
     
-        botom0.onclick = () => {
-          operador1.inc(0);
-          console.log(operador1)
-        }
-        botom1.onclick = () => {
-          operador1.inc(1);
-        }
-        botom2.onclick = () => {
-          operador1.inc(2);
-        }
-        botom3.onclick = () => {
-          operador1.inc(3);
-        }
-        botom4.onclick = () => {
-          operador1.inc(4);
-        }
-        botom5.onclick = () => {
-          operador1.inc(5);
-        }
-        botom6.onclick = () => {
-          operador1.inc(6);
-        }
-        botom7.onclick = () => {
-          operador1.inc(7);
-        }
-        botom8.onclick = () => {
-          operador1.inc(8);
-        }
-        botom9.onclick = () => {
-          operador1.inc(9);
-        }
 
-    mas.onclick=()=>{
-          botom0.onclick = () => {
-            operador2.inc(0);
-            console.log(operador2)
-          }
-          botom1.onclick = () => {
-            operador2.inc(1);
-          }
-          botom2.onclick = () => {
-            operador2.inc(2);
-          }
-          botom3.onclick = () => {
-            operador2.inc(3);
-          }
-          botom4.onclick = () => {
-            operador2.inc(4);
-          }
-          botom5.onclick = () => {
-            operador2.inc(5);
-          }
-          botom6.onclick = () => {
-            operador2.inc(6);
-          }
-          botom7.onclick = () => {
-            operador2.inc(7);
-          }
-          botom8.onclick = () => {
-            operador2.inc(8);
-          }
-          botom9.onclick = () => {
-            operador2.inc(9);
+    igual.onclick=()=>{
+        if(cadena[0]==""){
+            cadena[0]='0';}
+        cadena[2]= operador2.valor;
+        console.log(cadena)
+        if(cadena[1] == '+'){
+            var numero= (parseInt(cadena[0]) + parseInt(cadena[2]))
+            display.innerHTML= numero;
+            operador1.valor= numero;
+            operador2.valor= '';
         }
-      }
-      igual.onclick=()=>{
-          var numero= (parseInt(operador1.valor) + parseInt(operador2.valor))
-           display.innerHTML= numero;
-           operador1.valor= numero;
-           operador2.valor= '';
+        if(cadena[1] == '-'){
+            var numero= (parseInt(cadena[0]) - parseInt(cadena[2]))
+            display.innerHTML= numero;
+            operador1.valor= numero;
+            operador2.valor= '';
+        }
+        if(cadena[1] == 'x'){
+            var numero= (parseInt(cadena[0]) * parseInt(cadena[2]))
+            display.innerHTML= numero;
+            operador1.valor= numero;
+            operador2.valor= '';
+        }
        }
-      ac.onclick=()=>{
-          document.location.reload();
-        }
+    ac.onclick=()=>{
+        document.location.reload();
+    }
 }
